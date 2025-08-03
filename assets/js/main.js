08cbf4bf30bb11906607f14dc55d15b84a6f2406
hasan071 <hasanahmedaboss@gmail.com>
2025-08-04 01:23:25 +0300

    new update

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,27 +8,66 @@ let buttons = document.querySelectorAll(".bt, .Mbt, .n, .mm, .m");
 let equal = document.querySelectorAll(".eq")[0]
 buttons.forEach(function (el){
         el.onclick = function(){
+            const operators = [ "*" , "/" , "+" , "-" , "%" ]
+            const lastChar = input.value.slice(-1)
+            if ( operators.includes(lastChar) && operators.includes(el.innerText) ){
+                return;
+            }
             if ( el==document.querySelector(".Mbt") ){
-                input.value = 0
+                input.value = ""
             }
             else if ( el==document.querySelectorAll(".Mbt")[1] ){
-                    input.value = input.value.slice(0,-1)
+                input.value = input.value.slice(0,-1)
+            }
+            else if ( el==document.querySelector(".sin") ){
+                input.value = Math.sin(eval(input.value)*(Math.PI/180)).toFixed(1)
+            }
+            else if ( el==document.querySelector(".cos") ){
+                input.value = Math.cos(eval(input.value)*(Math.PI/180)).toFixed(1)
+            }
+            else if ( el==document.querySelector(".tan") ){
+                input.value = Math.tan(eval(input.value)*(Math.PI/180)).toFixed(1)
             }
             else {
-                if ( input.value==0 ){
-                    input.value = el.innerHTML
-                }else {
                     input.value += el.innerHTML
-                }
             }
+
+            
+
+            buttons.forEach(function(ell){
+                ell.style.border = "1px solid white"
+            })
+            el.style.border = "3px solid rgb(0, 180, 180)"
         }
 })
 
 equal.onclick = function(){
-    input.value = eval(input.value)
-}
-
+    if ( input.value=="" ){
+        input.value = 0
+    }
+    else {
+        input.value = eval(input.value)
+    }
+    buttons.forEach(function(ell){
+                ell.style.border = "1px solid white"
+            })
+            el.style.border = "3px solid rgb(0, 180, 180)"
+        }
+/*
+equal.forEach(function(el){
+    el.onclick = function(){
+        if ( el==document.querySelectorAll(".op") ){
+            input.value = ((parseFloat(el.innerHTML))*(3.14/180))
+        }
+    }
+})
+*/
 // console.log(document.querySelectorAll(".Mbt")[0]);
 // console.log(document.querySelectorAll(".Mbt")[1]);
 // console.log(document.querySelectorAll(".eq")[0]);
+// let tans = tan(45)
+// console.log(document.querySelectorAll(".op"));
+// console.log(document.querySelectorAll(".sin"));
+
+
 
